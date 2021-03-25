2f88215bfd3c3bed3fc0b48ebd6a7ce87318c0f0
Ruth Hinkle <ruth.e.hinkle@gmail.com>
2021-03-25 16:26:55 -0500

    added column variable, started append for table data

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Create Variables
+var button = d3.select("#filter-btn")
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+//Function to build table
+function 
+    tbody.html("");
+
+
+tableData.forEach(
+    .append("td")
+)
+//Function to handle click and filter by date (which rebuilds table using filtered data)
+const tbody = d3.select("tbody")
