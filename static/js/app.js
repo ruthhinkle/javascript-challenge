ee52e29b1a0ca3a511276a4f905482df11da3716
Ruth Hinkle <ruth.e.hinkle@gmail.com>
2021-04-01 16:20:49 -0500

    added most of app.js code

--- static/js/app.js
+++ static/js/app.js
@@ -5,15 +5,29 @@ var tableData = data;
 
 // Create Variables
 var button = d3.select("#filter-btn")
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
 
 //Function to build table
-function 
+function buildTable(tableData) {
+    var tbody = d3.select("tbody");
     tbody.html("");
+    tableData.forEach((rowData) => {
+        var row = tbody.append("tr");
+        Object.values(rowData).forEach((value) => {
+            var cells = row.append("td");
+            cells.text(value);
+        });
+    });
+}
 
+buildTable(tableData);
 
-tableData.forEach(
-    .append("td")
-)
 //Function to handle click and filter by date (which rebuilds table using filtered data)
-const tbody = d3.select("tbody")
+function clickButton() {
+    var dateTime = d3.select("#datetime").property("value");
+    var filteredTable = tableData;
+    filteredTable = filteredTable.filter(dataRow => dataRow.datetime === dateTime);
+    buildTable(filteredTable);
+}
+// Click button 
+button.on("click", clickButton);
